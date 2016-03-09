1b66f13a39f2963ab68b0531a5dc0a4cede82119
Joeri Gantois <info@JO3RI.be>
2016-03-09 10:04:38 +0100

    added basic sketch template

---
+++ ZMBT_DEV/ZMBT_DEV.ino
@@ -0,0 +1,60 @@
+/*
+ Block Hop: http://www.team-arg.org/zmbt-manual.html
+
+ Arduboy version 0.1:  http://www.team-arg.org/zmbt-downloads.html
+
+ MADE by TEAM a.r.g. : http://www.team-arg.org/more-about.html
+
+ 2016 - JO3RI - FUOPY
+
+ Game License: MIT : https://opensource.org/licenses/MIT
+
+ */
+
+//determine the game
+#define GAME_ID 40
+
+#include <Arduboy.h>
+#include <ArduboyExtra.h>
+#include <sprites.h>
+#include <simple_buttons.h>
+#include "menu.h"
+#include "game.h"
+
+Arduboy arduboy;
+
+byte gameState;
+
+typedef void (*FunctionPointer) ();
+FunctionPointer mainGameLoop[] =
+{
+  stateMenuIntro,
+  stateMenuMain,
+  stateMenuHelp,
+  stateMenuPlay,
+  stateMenuInfo,
+  stateMenuSoundfx,
+  stateGamePlaying,
+  stateGamePause,
+  stateGameOver,
+};
+
+void setup()
+{
+  arduboy.start();
+  arduboy.setFrameRate(60);
+  gameState = 0;
+  if (EEPROM.read(EEPROM_AUDIO_ON_OFF)) soundYesNo = true;
+  menuSelection = 3;
+}
+
+void loop() {
+  if (!(arduboy.nextFrame())) return;
+  buttons.poll();
+  if (soundYesNo == true) arduboy.audio.on();
+  else arduboy.audio.off();
+  arduboy.clearDisplay();
+  mainGameLoop[gameState]();
+  arduboy.display();
+}
+
---
+++ ZMBT_DEV/player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <Arduino.h>
+
+
+
+#endif
