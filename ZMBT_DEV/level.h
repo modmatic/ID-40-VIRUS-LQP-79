4284bd7b3924a13df1d278eaa47ceb15d92e24e8
JO3RI <info@JO3RI.be>
2016-03-19 00:48:01 +0100

    level ideas

--- ZMBT_DEV/level.h
+++ ZMBT_DEV/level.h
@@ -7,7 +7,16 @@ const unsigned char PROGMEM level01[] =
 {
   4, // amount of enemies
   2, // amount of survivors
+
+  // blocks used in the level
+  1,2,2,2,2,3,
+  4,5,5,5,5,6,
+  7,8,8,8,8,9,
+
 };
+
+
+
 
 
 #endif
