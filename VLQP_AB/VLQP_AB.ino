67e18441c352a7c193ccd1209a994302577ec687
JO3RI <info@JO3RI.be>
2016-04-05 20:13:52 +0200

    moved the pause function (not used)

--- VLQP_AB/VLQP_AB.ino
+++ VLQP_AB/VLQP_AB.ino
@@ -1,15 +1,15 @@
 /*
- VIRUS LQP-79: http://www.team-arg.org/zmbt-manual.html
+  VIRUS LQP-79: http://www.team-arg.org/zmbt-manual.html
 
- Arduboy version 0.8:  http://www.team-arg.org/zmbt-downloads.html
+  Arduboy version 0.8:  http://www.team-arg.org/zmbt-downloads.html
 
- MADE by TEAM a.r.g. : http://www.team-arg.org/more-about.html
+  MADE by TEAM a.r.g. : http://www.team-arg.org/more-about.html
 
- 2016 - FUOPY - JO3RI - STG - CASTPIXEL - JUSTIN CRY
+  2016 - FUOPY - JO3RI - STG - CASTPIXEL - JUSTIN CRY
 
- Game License: MIT : https://opensource.org/licenses/MIT
+  Game License: MIT : https://opensource.org/licenses/MIT
 
- */
+*/
 
 //determine the game
 #define GAME_ID 40
@@ -44,8 +44,8 @@ void setup()
 {
   arduboy.start();
   arduboy.setFrameRate(60);
-  gameState = 0;
   if (EEPROM.read(EEPROM_AUDIO_ON_OFF)) soundYesNo = true;
+  gameState = 0;
   menuSelection = 3;
 }
 
