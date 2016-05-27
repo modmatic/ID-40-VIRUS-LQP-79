8d61d095fd9123a7efd7522ce7ce081417315b0a
Joeri Gantois <info@JO3RI.be>
2016-05-27 16:23:36 +0200

    changed the door to arrows

--- VLQP_AB/door.cpp
+++ VLQP_AB/door.cpp
@@ -7,12 +7,19 @@
 Door exitDoor;
 
 // method implementations ////////////////////////////////////////////////////
+byte checkDoorOrientation()
+{
+  if (exitDoor.y == 0) return EXIT_FACING_SOUTH;
+  if (exitDoor.x == 62) return EXIT_FACING_WEST;
+  if (exitDoor.y == 30) return EXIT_FACING_NORTH;
+  if (exitDoor.x == 0) return EXIT_FACING_EAST;
+}
 
 void setDoorPosition(int x, int y)
 {
   exitDoor.x = x;
   exitDoor.y = y;
-  
+  exitDoor.orientation = checkDoorOrientation();
   exitDoor.active = false;
 }
 
@@ -25,13 +32,24 @@ bool checkDoorCollision()
 {
   return
     ( exitDoor.active ) &&
-    ( exitDoor.x < coolGirl.x+PLAYER_WIDTH ) &&
+    ( exitDoor.x < coolGirl.x + PLAYER_WIDTH ) &&
     ( exitDoor.x + SURVIVOR_WIDTH > coolGirl.x ) &&
-    ( exitDoor.y < coolGirl.y+PLAYER_HEIGHT ) &&
+    ( exitDoor.y < coolGirl.y + PLAYER_HEIGHT ) &&
     ( exitDoor.y + SURVIVOR_HEIGHT > coolGirl.y );
 }
 
 void drawDoor()
 {
-  if(exitDoor.active) sprites.drawPlusMask(exitDoor.x - mapPositionX, exitDoor.y - mapPositionY,door_plus_mask, 0);
+  if (exitDoor.active)
+  {
+    if (arduboy.everyXFrames(10))exitDoor.frame++;
+    if (exitDoor.frame > 3)exitDoor.frame = 0;
+    for (byte x = 0; x < 2; x++)
+    {
+      for (byte y = 0; y < 2; y++)
+      {
+        sprites.drawPlusMask(exitDoor.x - mapPositionX +(x*8), exitDoor.y - mapPositionY + (y*8), exit_plus_mask, exitDoor.frame + 4 * exitDoor.orientation);
+      }
+    }
+  }
 }
--- VLQP_AB/door.h
+++ VLQP_AB/door.h
@@ -5,8 +5,13 @@
 
 // constants /////////////////////////////////////////////////////////////////
 
-#define DOOR_WIDTH 16
-#define DOOR_HEIGHT 16
+#define DOOR_WIDTH              16
+#define DOOR_HEIGHT             16
+
+#define EXIT_FACING_SOUTH       0
+#define EXIT_FACING_WEST        1
+#define EXIT_FACING_NORTH       2
+#define EXIT_FACING_EAST        3
 
 // structures ////////////////////////////////////////////////////////////////
 
@@ -14,6 +19,8 @@ struct Door {
   int x;
   int y;
   byte active;
+  byte orientation;
+  byte frame;
 };
 
 // globals ///////////////////////////////////////////////////////////////////
